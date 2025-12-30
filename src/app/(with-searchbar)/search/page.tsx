f24755b6519c19e2fa62082b53e23fa269209eab
Steady-K <tkdlxm56@gmail.com>
2025-12-30 17:48:09 +0900

    feat(ui): make movie skeleton UI responsive

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -3,6 +3,7 @@ import styles from "./page.module.css";
 import { MovieData } from "@/types";
 import { delay } from "@/util/delay";
 import { Suspense } from "react";
+import MovieListSkeleton from "@/components/skeleton/movie-list-skeleton";
 
 async function SearchResult({ q }: { q: string }) {
   await delay(1500);
@@ -31,7 +32,10 @@ export default async function Page({
 }) {
   const { q } = await searchParams;
   return (
-    <Suspense key={q || ""} fallback={<div>Loading...</div>}>
+    <Suspense
+      key={q || ""}
+      fallback={<MovieListSkeleton count={3} variant="reco" />}
+    >
       <SearchResult q={q || ""} />
     </Suspense>
   );
